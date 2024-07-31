99e6fb8240e78278d449ebc89e223844b88da001
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-07-30 23:05:43 -0300

    Update cadeias.rb

--- conceitos-essenciais/cadeias.rb
+++ conceitos-essenciais/cadeias.rb
@@ -17,10 +17,37 @@ Usar // como argumento do método split() permite que ele retorne um Array conte
 
 cadeia_exemplo_2 = "abc".split(//)
 
-# Comprimento de Cadeia
+# Comprimento de cadeia
 
 =begin
 O comprimento de uma cadeia r, denotado por |r|, pode ser codificado em Ruby pela operação length() sobre a
-instância da classe Array que representa a cadeia r.
+instância da classe Array que representa a cadeia r. Uma outra alternativa seria usar a operação size().
 =end
 
+r = "abc".split(//)
+r.length()
+r.size()
+
+# Cadeia vazia
+
+=begin
+Uma cadeia vazia não contém símbolos, uma instância de classe Array sem elementos pode ser utilizada para
+representar ela.
+=end
+
+cadeia_vazia = []
+
+# Concatenação de cadeias
+
+=begin
+A concatenação das cadeias r e s é formada pela justaposição dos símbolos de r seguidos pelos símbolos de s.
+O operador + da classe Array pode ser usado para codificar a concatenação de cadeias em Ruby.
+=end
+
+r = "abc".split(//)
+s = "dbaca".split(//)
+t = "a".split(//)
+r + s
+s + r
+r + t
+t + s
