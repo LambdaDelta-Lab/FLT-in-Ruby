430d4004d294051ce425e171dda663bb9e0c79d9
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-02 23:33:14 +0000

    Create Reconhecedor.rb

---
+++ automatos/Reconhecedor.rb
@@ -0,0 +1,40 @@
+require 'Automato.rb'
+
+class Reconhecedor
+  # Estrutura de colaboradores
+  attr_accessor :automato
+
+  # Interface de instanciação
+  def initialize(estadoInicial, estadosFinais)
+    instanciarEstruturaEspecifica()
+    instanciarAutomato(estadoInicial, estadosFinais)
+  end
+
+  def instanciarEstruturaEspecifica()
+  end
+
+  def instanciarAutomato(estadoInicial, estadosFinais)
+    raise NotImplementedError.new("'instanciarAutomato()' não pode ser executado.")
+  end
+
+  # Interface característica
+  def iniciar(cadeia)
+    @automato.iniciar(cadeia)
+  end
+
+  def analisar()
+    @subEspacoAutomatos = @automato.executar()
+  end
+
+  def reconheceu?()
+    reconheci = false
+    @subEspacoAutomatos.each do |automato|
+      reconheci = true if (condicaoDoAutomato?(automato))
+    end
+    return reconheci
+  end
+
+  def condicaoDoAutomato?(automato)
+    return (automato.consulta.atingiuEOF?() && automato.consulta.estaEmEstadoFinal?())
+  end
+end
