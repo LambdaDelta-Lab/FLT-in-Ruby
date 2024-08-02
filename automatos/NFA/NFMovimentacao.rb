29219c7fc2ce5178b7902e5712533dd945173d4c
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-02 21:10:54 +0000

    Update NFMovimentacao.rb

--- automatos/NFA/NFMovimentacao.rb
+++ automatos/NFA/NFMovimentacao.rb
@@ -1,1 +1,27 @@
+require '../Movimentacao.rb'
 
+class NFMovimentacao < Movimentacao
+  ondaDeClones = {}
+  condicaoCorrente = [@automato.consulta.estadoCorrente?(),
+                      @automato.entrada.ler()]
+  @automato.transicoes.each do |condicaoEsperada, estadosSeguintes|
+    if (condicaoEsperada == condicaoCorrente)
+      estadosSeguintes.each do |estadoSeguinte|
+        clone = @automato.clonar()
+        ondaDeClones[clone] = estadoSeguinte
+      end
+    end
+  end
+  return ondaDeClones
+end
+
+def mover(proximoEstado)
+  @automato.estadoCorrente = proximoEstado
+  @automato.entrada.avancar()
+end
+
+def adicionarTransicao(transicao)
+  @transicoes.update(transicao)
+end
+
+end
