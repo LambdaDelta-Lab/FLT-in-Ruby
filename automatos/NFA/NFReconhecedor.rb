131aaf06312225e99a46e3a6d10805c39a9e96a6
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-02 21:17:18 +0000

    Update NFReconhecedor.rb

--- automatos/NFA/NFReconhecedor.rb
+++ automatos/NFA/NFReconhecedor.rb
@@ -1,1 +1,8 @@
+require '../Reconhecedor.rb'
+require 'NFAutomata.rb'
 
+class NFReconhecedor < Reconhecedor
+  def instanciarAutomato(estadoInicial, estadosFinais)
+    @automato = NFAutomata.new(estadoInicial, estadosFinais)
+  end
+end
