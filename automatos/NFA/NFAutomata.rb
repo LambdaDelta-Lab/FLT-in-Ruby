df09be2bc402730ee9b564005e11f9d863e0fd5f
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-02 21:04:53 +0000

    Update NFAutomata.rb

--- automatos/NFA/NFAutomata.rb
+++ automatos/NFA/NFAutomata.rb
@@ -2,3 +2,12 @@ require '../Automata.rb'
 require 'FitaLimitada.rb'
 require 'NFMovimentacao.rb'
 
+class NFAutomata < Automata
+  def instanciarEntrada()
+    @entrada = FitaLimitada.new()
+  end
+
+  def instanciarMovimentacao()
+    @movimentacao = NFMovimentacao.new(self)
+  end
+end
