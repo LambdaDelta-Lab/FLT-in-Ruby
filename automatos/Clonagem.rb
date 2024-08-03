06c07508ca2bc461fd3bf0a3074dc7dfa5e61d92
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-03 14:35:54 +0000

    Update Clonagem.rb

--- automatos/Clonagem.rb
+++ automatos/Clonagem.rb
@@ -3,11 +3,12 @@ require 'yaml'
 class Clonagem
   def clonar(original)
     dadosDoClone = "#{original.class}.dat"
-    open(dadosDoClone, 'w') { |arquivo|
+    open(dadosDoClone, 'w') do |arquivo|
       YAML.dump(original, arquivo)
-    }
-    clone = open(dadosDoClone) { |arquivo|
-      YAML.load(arquivo)
-    }
+    end
+    clone = open(dadosDoClone) do |arquivo|
+      YAML.safe_load(arquivo, permitted_classes: [original.class, DFAutomata, FitaLimitada, DFMovimentacao, Consulta], aliases: true)
+    end
+    clone
   end
 end
