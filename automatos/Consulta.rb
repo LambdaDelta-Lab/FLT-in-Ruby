547d51a4de6bbf169073b776bd4f6dbd473e6bd0
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-03 14:45:49 +0000

    Update Consulta.rb

--- automatos/Consulta.rb
+++ automatos/Consulta.rb
@@ -1,1 +1,28 @@
+class Consulta
+  # Estrutura de colaboradores
+  attr_accessor :automato
 
+  # Interface de instanciação
+  def initialize(automato)
+    @automato = automato
+  end
+
+  # Interface de consulta
+  def estadoCorrente?()
+    return @automato.estadoCorrente
+  end
+
+  def atingiuEOF?()
+    return @automato.entrada.atingiuEOF?()
+  end
+
+  def configuracao?()
+    estado = estadoCorrente?()
+    configuracaoFita = @automato.entrada.configuracao?()
+    return "(#{estado}, #{configuracaoFita})"
+  end
+
+  def estaEmEstadoFinal?()
+    return @automato.estadosFinais.include?(estadoCorrente?())
+  end
+end
