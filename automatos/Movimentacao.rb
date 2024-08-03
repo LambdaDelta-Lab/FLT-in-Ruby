ff36790503cc36c4404a19113af64b0319c78f0d
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-03 14:44:02 +0000

    Update Movimentacao.rb

--- automatos/Movimentacao.rb
+++ automatos/Movimentacao.rb
@@ -1,1 +1,22 @@
+class Movimentacao
+  # Estrutura de colaboradores
+  attr_accessor :automato
 
+  # Interface de instanciação
+  def initialize(automato)
+    @automato = automato
+  end
+
+  # Interface característica
+  def executar()
+    raise NotImplementedError.new("'executar()' deve ser implementado nas subclasses.")
+  end
+
+  def calcularOndaDeClones()
+    raise NotImplementedError.new("'calcularOndaDeClones()' deve ser implementado nas subclasses.")
+  end
+
+  def mover(proximoEstado)
+    raise NotImplementedError.new("'mover()' deve ser implementado nas subclasses.")
+  end
+end
