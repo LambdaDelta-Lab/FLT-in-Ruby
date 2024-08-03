2685a50a45f639d5054ec27bdc12d57da8717e22
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-03 01:23:42 +0000

    Update DFAutomata.rb

--- automatos/DFA/DFAutomata.rb
+++ automatos/DFA/DFAutomata.rb
@@ -3,3 +3,17 @@ require '../FitaLimitada.rb'
 require 'DFMovimentacao.rb'
 
 class DFAutomata.rb < Automato
+  # Interface de instanciação
+  def instanciarEntrada()
+    @entrada = FitaLimitada.new()
+  end
+
+  def instanciarMovimentacao()
+    @movimentacao = DFMovimentacao.new(self)
+  end
+
+  # Interface de ajuste estrutural
+  def adicionarTransicao(transicao)
+    @transicoes.update(transicao)
+  end
+end
