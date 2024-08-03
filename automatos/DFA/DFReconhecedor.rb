747dc6b85817d17f7656a1d8104b5b12654fd0e2
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-03 15:18:57 +0000

    Update DFReconhecedor.rb

--- automatos/DFA/DFReconhecedor.rb
+++ automatos/DFA/DFReconhecedor.rb
@@ -1,5 +1,5 @@
-require File.expand_path('~Documents/Ruby/Reconhecedor.rb')
-require File.expand_path('~Documents/Ruby/DFA/DFAutomata.rb')
+require File.expand_path('~/Documents/Ruby/Reconhecedor.rb')
+require File.expand_path('~/Documents/Ruby/DFA/DFAutomata.rb')
 
 class DFReconhecedor < Reconhecedor
   def instanciarAutomato(estadoInicial, estadosFinais)
